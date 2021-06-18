8f97c02477cfa19274d7ba4aacba5bb96bce088d
Dominik Alfery <alferdom@fit.cvut.cz>
2021-06-18 15:51:13 +0200

    Signed-off-by: Dominik Alfery <alferdom@fit.cvut.cz>

--- Semestral/src/CApplication.cpp
+++
@@ -1,9 +0,0 @@
-#include "CApplication.h"
-
-
-CApplication::CApplication(CInterface &interface) : myInterface(std::move(interface)) {}
-
-void CApplication::Run() const{
-    myInterface.StartDisplay();
-    myInterface.MakeMenu();
-}
