6e374575310640111a3c2294c794c05a2213aeb1
Dominik Alfery <alferdom@fit.cvut.cz>
2023-02-21 20:52:32 +0100

    empty repo

--- OpenGL/alferdom/inc/initFunctions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "alferdom.h"
-
-GLFWwindow* initGLFW(const ConfigLoader& configLoader);
-
-void initImGui(GLFWwindow* window, const char* shaderVersion);
-
-void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
